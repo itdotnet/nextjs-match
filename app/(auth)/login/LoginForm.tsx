2e0f43d84150f30bd6fd787b0c1bec18ddfe8fc4
omid <itmanager.abbasi@gmail.com>
2024-12-03 12:18:27 +0330

    signing user part 2

--- app/(auth)/login/LoginForm.tsx
+++ app/(auth)/login/LoginForm.tsx
@@ -1,20 +1,31 @@
 "use client";
 
+import { signInUser } from "@/app/actions/authActions";
 import { loginSchema, LoginSchema } from "@/lib/schemas/loginSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button, Card, CardBody, CardHeader, Input } from "@nextui-org/react";
+import { useRouter } from "next/navigation";
 import React from "react";
 import { useForm } from "react-hook-form";
 import { GiPadlock } from "react-icons/gi";
 
 const LoginForm = () => {
-  const { register, handleSubmit, formState: { errors, isValid } } = useForm<LoginSchema>({
+  const router=useRouter();
+
+  const { register, handleSubmit, formState: { errors, isValid,isSubmitting } } = useForm<LoginSchema>({
     resolver: zodResolver(loginSchema),
     mode: "onTouched"
   });
 
-  const onSubmit = (data: LoginSchema) => {
-    console.log(data);
+  const onSubmit =async (data: LoginSchema) => {
+    const result=await signInUser(data);
+
+    if(result.status==="success"){
+      router.push('/members');
+    }
+    else{
+      console.log(result.error);
+    }
   }
 
   return (
@@ -35,7 +46,7 @@ const LoginForm = () => {
               isInvalid={!!errors.email} errorMessage={errors.email?.message as string} />
             <Input defaultValue="" label="Password" variant="bordered" type="Password" {...register("password")}
               isInvalid={!!errors.password} errorMessage={errors.password?.message as string} />
-            <Button fullWidth color="secondary" type="submit" isDisabled={!isValid}>Login</Button>
+            <Button isLoading={isSubmitting} fullWidth color="secondary" type="submit" isDisabled={!isValid}>Login</Button>
           </div>
         </form>
       </CardBody>
