dcc44002787f4380b9c85e6c18ced13a17a9c495
itdotnet <itmanager.abbasi@gmail.com>
2024-12-07 07:00:36 +0000

    fix cross origin problem and show usermenu dropdown

--- components/navbar/UserMenu.tsx
+++ components/navbar/UserMenu.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { signOutUser } from '@/app/actions/authActions'
 import { Avatar, Dropdown, DropdownItem, DropdownMenu, DropdownSection, DropdownTrigger } from '@nextui-org/react'
 import { Session } from 'next-auth'
@@ -27,13 +29,13 @@ const UserMenu = ({user}:Props) => {
                 <DropdownItem isReadOnly as='span' className='h-14 flex flex-row' aria-label='username'>
                     Signed in as {user?.name}
                 </DropdownItem>
-                <DropdownItem as={Link} href="/members/edit">
-                    Edit Profile
-                </DropdownItem>
-                <DropdownItem color="danger" onClick={async()=>signOutUser()}>
-                    Log out
-                </DropdownItem>
             </DropdownSection>
+            <DropdownItem as={Link} href="/members/edit">
+                Edit Profile
+            </DropdownItem>
+            <DropdownItem color="danger" onClick={async()=>signOutUser()}>
+                Log out
+            </DropdownItem>
         </DropdownMenu>
     </Dropdown>
   )
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,31 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        'localhost:3000',
+        'cautious-funicular-jj6wv454xg7359qv-3000.app.github.dev',
+      ]
+    }
+  }
 };
 
 export default nextConfig;
+
+// @ts-check
+ 
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   /* config options here */
+//     experimental: {
+//     serverActions: {
+//       allowedOrigins: [
+//         'localhost:3000',
+//         'cautious-funicular-jj6wv454xg7359qv-3000.app.github.dev',
+//       ]
+//     }
+//   }
+// }
+ 
+// module.exports = nextConfig
