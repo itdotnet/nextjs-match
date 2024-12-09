7895dfa1fd288d4a3bba473ae90f4dbf45d7a7a2
itdotnet <itmanager.abbasi@gmail.com>
2024-12-09 11:54:43 +0000

    styling to cards

--- app/members/MemberCard.tsx
+++ app/members/MemberCard.tsx
@@ -1,5 +1,6 @@
 import { Card, CardFooter, Image } from '@nextui-org/react'
 import { Member } from '@prisma/client'
+import Link from 'next/link'
 import React from 'react'
 
 type Props={
@@ -8,7 +9,12 @@ type Props={
 
 const MemberCard = ({member}:Props) => {
   return (
-    <Card fullWidth>
+    <Card 
+    fullWidth
+    as={Link}
+    href={`/members/${member.userId}`}
+    isPressable
+    >
         <Image 
             isZoomed
             width={300}
@@ -16,7 +22,7 @@ const MemberCard = ({member}:Props) => {
             src={member.image || '/images/user.png'}
             className='aspect-square object-cover'
         />
-        <CardFooter>
+        <CardFooter className='flex justify-start bg-black overflow-hidden absolute bottom-0 z-10 bg-dark-gradient'>
             <div className="flex flex-col text-white">
                 <span className='font-semibold'>{member.name}</span>
                 <span className='text-sm'>{member.city}</span>
