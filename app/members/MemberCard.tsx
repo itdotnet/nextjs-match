973e3149dcb8ed27c03096ae9097e1366c1397b8
itdotnet <itmanager.abbasi@gmail.com>
2024-12-09 11:59:14 +0000

    show age of members

--- app/members/MemberCard.tsx
+++ app/members/MemberCard.tsx
@@ -1,3 +1,4 @@
+import { calculateAge } from '@/lib/util'
 import { Card, CardFooter, Image } from '@nextui-org/react'
 import { Member } from '@prisma/client'
 import Link from 'next/link'
@@ -24,7 +25,7 @@ const MemberCard = ({member}:Props) => {
         />
         <CardFooter className='flex justify-start bg-black overflow-hidden absolute bottom-0 z-10 bg-dark-gradient'>
             <div className="flex flex-col text-white">
-                <span className='font-semibold'>{member.name}</span>
+                <span className='font-semibold'>{member.name}, {calculateAge(member.dateOfBirth)}</span>
                 <span className='text-sm'>{member.city}</span>
             </div>
         </CardFooter>
