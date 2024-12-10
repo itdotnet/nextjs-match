2f9ef367182d00c6c06ad9ed708675001a02a614
itdotnet <itmanager.abbasi@gmail.com>
2024-12-10 07:31:17 +0000

    add layout to single member page

--- app/members/[userId]/page.tsx
+++ app/members/[userId]/page.tsx
@@ -1,10 +1,11 @@
 import { getMemberById } from '@/app/actions/memberActions'
+import { notFound } from 'next/navigation';
 import React from 'react'
 
 const MemberDetailedPage =async ({params}:{params:{userId:string}}) => {
     const member=await getMemberById(params.userId);
 
-    if(!member) return null;
+    if(!member) return notFound();
 
   return (
     <div>{member.name}</div>
