095ae9e59d299fc91ab329f19c4caa94daaa9722
omid <itmanager.abbasi@gmail.com>
2024-12-11 17:20:37 +0330

    Add the list tabs

--- app/lists/page.tsx
+++ app/lists/page.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { fetchCurrentUserLikeIds, fetchLikedMembers } from '../actions/like.actions'
+import ListsTab from './ListsTab';
 
-const ListsPage = () => {
+const ListsPage =async ({searchParams}:{searchParams:{type:string}}) => {
+    const likeIds=await fetchCurrentUserLikeIds();
+    const members=await fetchLikedMembers(searchParams.type);
+
     return (
-        <div>ListsPage</div>
+        <div>
+            <ListsTab members={members!} likeIds={likeIds}/>
+        </div>
     )
 }
 
