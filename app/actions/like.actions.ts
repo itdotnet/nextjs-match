1c4d5be0f5fd9b4e19aec2fc8aa327b23c92e478
omid <itmanager.abbasi@gmail.com>
2024-12-11 14:34:38 +0330

    Add the list actions

--- app/actions/like.actions.ts
+++ app/actions/like.actions.ts
@@ -38,14 +38,73 @@ export async function fetchCurrentUserLikeIds() {
             where: {
                 sourceUserId: userId
             },
-            select:{
-                targetUserId:true
+            select: {
+                targetUserId: true
             }
         })
 
-        return likeIds.map(like=>like.targetUserId);
+        return likeIds.map(like => like.targetUserId);
     } catch (error) {
         console.log(error);
         throw error;
     }
 }
+
+export async function fetchLikedMembers(type = "source") {
+    try {
+        const userId = await getAuthUserId();
+
+        switch (type) {
+            case 'source':
+                return await fetchSourceLikes(userId);
+            case 'target':
+                return await fetchTargetLikes(userId);
+            case 'mutual':
+                return await fetchMutualLikes(userId);
+            default:
+                break;
+        }
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+async function fetchSourceLikes(userId: string) {
+    const sourceList = await prisma.like.findMany({
+        where: { sourceUserId: userId },
+        select: { targetMember: true }
+    })
+
+    return sourceList.map(x => x.targetMember);
+}
+async function fetchTargetLikes(userId: string) {
+    const targetList = await prisma.like.findMany({
+        where: { targetUserId: userId },
+        select: { sourceMember: true }
+    })
+
+    return targetList.map(x => x.sourceMember);
+}
+
+async function fetchMutualLikes(userId: string) {
+    const likedUsers = await prisma.like.findMany({
+        where: { sourceUserId: userId },
+        select: { targetUserId: true }
+    });
+
+    const likedIds=likedUsers.map(x=>x.targetUserId);
+
+    const mutualList = await prisma.like.findMany({
+        where: {
+            AND: [
+                {targetUserId: userId},
+                {sourceUserId:{in:likedIds}}
+            ]
+        },
+        select:{sourceMember:true}
+    });
+
+    return mutualList.map(x => x.sourceMember);
+}
+
