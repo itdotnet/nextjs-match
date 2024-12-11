3f82c6847a25f92abc4bcb2e0787047a50f3c843
omid <itmanager.abbasi@gmail.com>
2024-12-11 13:46:53 +0330

    Fetch the likes

--- app/members/MemberCard.tsx
+++ app/members/MemberCard.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import LikeButton from '@/components/LikeButton'
 import { calculateAge } from '@/lib/util'
 import { Card, CardFooter, Image } from '@nextui-org/react'
@@ -5,36 +7,46 @@ import { Member } from '@prisma/client/wasm'
 import Link from 'next/link'
 import React from 'react'
 
-type Props={
-    member:Member
+type Props = {
+    member: Member
+    likeIds: string[]
 }
 
-const MemberCard = ({member}:Props) => {
-  return (
-    <Card 
-    fullWidth
-    as={Link}
-    href={`/members/${member.userId}`}
-    isPressable
-    >
-        <Image 
-            isZoomed
-            width={300}
-            alt={member.name}
-            src={member.image || '/images/user.png'}
-            className='aspect-square object-cover'
-        />
-        <div className='absolute top-3 right-3 z-50'>
-            <LikeButton targetUserId={member.userId} hasLiked={false}/>
-        </div>
-        <CardFooter className='flex justify-start bg-black overflow-hidden absolute bottom-0 z-10 bg-dark-gradient'>
-            <div className="flex flex-col text-white">
-                <span className='font-semibold'>{member.name}, {calculateAge(member.dateOfBirth)}</span>
-                <span className='text-sm'>{member.city}</span>
+const MemberCard = ({ member, likeIds }: Props) => {
+    const hasLiked = likeIds.includes(member.userId);
+
+    const preventLinkAction = (e: React.MouseEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+    }
+
+    return (
+        <Card
+            fullWidth
+            as={Link}
+            href={`/members/${member.userId}`}
+            isPressable
+        >
+            <Image
+                isZoomed
+                width={300}
+                alt={member.name}
+                src={member.image || '/images/user.png'}
+                className='aspect-square object-cover'
+            />
+            <div onClick={preventLinkAction}>
+                <div className='absolute top-3 right-3 z-50'>
+                    <LikeButton targetUserId={member.userId} hasLiked={hasLiked} />
+                </div>
             </div>
-        </CardFooter>
-    </Card>
-  )
+            <CardFooter className='flex justify-start bg-black overflow-hidden absolute bottom-0 z-10 bg-dark-gradient'>
+                <div className="flex flex-col text-white">
+                    <span className='font-semibold'>{member.name}, {calculateAge(member.dateOfBirth)}</span>
+                    <span className='text-sm'>{member.city}</span>
+                </div>
+            </CardFooter>
+        </Card>
+    )
 }
 
 export default MemberCard
