5201d97426ce2a100411e9aa5085593bf8fb18be
itdotnet <itmanager.abbasi@gmail.com>
2025-01-06 11:52:23 +0000

    show messages to user

---
+++ app/actions/messageActions.ts
@@ -0,0 +1,83 @@
+"use server";
+
+import { prisma } from "@/lib/prisma";
+import { getAuthUserId } from "./authActions";
+import { messageSchema, MessageSchema } from "@/lib/schemas/messageSchema";
+import { ActionResult } from "@/types";
+import { Message } from "@prisma/client";
+import { mapMessageToMessageDto } from "@/lib/mappings";
+
+export async function createMessage(recipientUserId:string,data:MessageSchema):Promise<ActionResult<Message>>{
+    try {
+        
+    const userId=await getAuthUserId();
+
+    const validated=messageSchema.safeParse(data);
+
+    if(!validated.success) return {status:'error',error:validated.error.errors};
+
+    const {text}=validated.data;
+
+    const message=await prisma.message.create({
+        data:{
+                text,
+                recipientId:recipientUserId,
+                senderId:userId
+            }
+    });
+
+    return {status:'success',data:message};
+    } catch (error) {
+        console.log(error);
+        return {status:'error',error:"Something went wrong"};   
+    }
+}
+
+export async function getMessageThread(recipientId:string){
+    try {
+        const userId=await getAuthUserId();
+
+        const messages=await prisma.message.findMany({
+            where:{
+                OR:[
+                    {
+                        senderId:userId,
+                        recipientId
+                    },
+                    {
+                        senderId:recipientId,
+                        recipientId:userId
+                    }
+                ]
+            },
+            orderBy:{
+                created:'asc'
+            },
+            select:{
+                id:true,
+                text:true,
+                created:true,
+                dateRead:true,
+                sender:{
+                    select:{
+                        userId:true,
+                        name:true,
+                        image:true
+                    }
+                },
+                recipient:{
+                    select:{
+                        userId:true,
+                        name:true,
+                        image:true
+                    }
+                }
+            }
+        });
+
+        return messages.map(message=>mapMessageToMessageDto(message));
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
--- app/members/[userId]/chat/page.tsx
+++ app/members/[userId]/chat/page.tsx
@@ -1,12 +1,27 @@
 import CardInnerWrapper from "@/components/CardInnerWrapper";
 import React from "react";
 import ChatForm from "./ChatForm";
+import { getMessageThread } from "@/app/actions/messageActions";
 
-const ChatPage = () => {
+const ChatPage =async ({params}:{params:{userId:string}}) => {
+  const messages=await getMessageThread(params.userId);
+
+  const body=(
+    <div>
+      {messages.length===0?'No messages to display':(
+        <div>
+          {messages.map(message=>(
+            <p key={message.id}>{message.text}</p>
+          ))}
+        </div>
+      )}
+    </div>
+  )
+
   return (
     <CardInnerWrapper
       header="Profile"
-      body={<div>Chat goes here</div>}
+      body={body}
       footer={<ChatForm />}
     />
   );
