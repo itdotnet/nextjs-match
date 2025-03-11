55330ec5b9e3a4c7a612fd702c29bf956cc3786d
omid <itmanager.abbasi@gmail.com>
2025-03-11 15:26:35 +0330

    seed data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,32 +2,32 @@ import { PrismaClient } from "@prisma/client";
 import { membersData } from "./membersData";
 import { hash } from "bcryptjs";
 
-const prisma=new PrismaClient();
+const prisma = new PrismaClient();
 
-async function seedMembers(){
-    return membersData.map(async member=>prisma.user.create({
-        data:{
-            email:member.email,
-            emailVerified:new Date(),
-            name:member.name,
-            passwordHash:await hash("506903",10),
-            image:member.image,
-            profileComplete:true,
-            member:{
-                create:{
-                    dateOfBirth:new Date(member.dateOfBirth),
-                    gender:member.gender,
-                    name:member.name,
-                    created:new Date(member.created),
-                    updated:new Date(member.lastActive),
-                    description:member.description,
-                    city:member.city,
-                    country:member.country,
-                    image:member.image,
-                    photos:{
-                        create:{
-                            url:member.image,
-                            isApproved:true
+async function seedMembers() {
+    return membersData.map(async member => prisma.user.create({
+        data: {
+            email: member.email,
+            emailVerified: new Date(),
+            name: member.name,
+            passwordHash: await hash("506903", 10),
+            image: member.image,
+            profileComplete: true,
+            member: {
+                create: {
+                    dateOfBirth: new Date(member.dateOfBirth),
+                    gender: member.gender,
+                    name: member.name,
+                    created: new Date(member.created),
+                    updated: new Date(member.lastActive),
+                    description: member.description,
+                    city: member.city,
+                    country: member.country,
+                    image: member.image,
+                    photos: {
+                        create: {
+                            url: member.image,
+                            isApproved: true
                         }
                     }
                 }
@@ -36,26 +36,28 @@ async function seedMembers(){
     }))
 }
 
-async function seedAdmin(){
+async function seedAdmin() {
     await prisma.user.create({
-        data:{
-            email:'admin@test.com',
-            emailVerified:new Date(),
-            name:'Admin',
-            passwordHash:await hash('506903admin',10),
-            role:'ADMIN'
+        data: {
+            email: 'admin@test.com',
+            emailVerified: new Date(),
+            name: 'Admin',
+            passwordHash: await hash('506903admin', 10),
+            role: 'ADMIN'
         }
     });
 }
 
 async function main() {
-    await seedMembers();
-    await seedAdmin();
+    if (process.env.RUN_SEED === 'true' || process.env.NODE_ENV === "development") {
+        await seedMembers();
+        await seedAdmin();
+    }
 }
 
-main().catch(e=>{
+main().catch(e => {
     console.error(e);
     process.exit(1);
-}).finally(async ()=>{
+}).finally(async () => {
     await prisma.$disconnect();
 })
